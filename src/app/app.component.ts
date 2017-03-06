243d91c0fbced66bf526bd821048ca3595020957
Montana Sawyer <montanarsawyer@gmail.com>
2017-03-05 20:46:54 -0800

    remove top menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <a routerLink="register">Register</a>
-    <a routerLink="report">Report</a>
-    <a routerLink="encounters">Encounters</a>
-    <a routerLink="">Welcome</a>
-    <a routerLink="**">Not Found</a>
     <div class="app-container"><router-outlet></router-outlet></div>
   `,
   styleUrls: ['./app.component.scss']
@@ -15,3 +10,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
 }
+
+
+
+// <a routerLink="register">Register</a>
+// <a routerLink="report">Report</a>
+// <a routerLink="encounters">Encounters</a>
+// <a routerLink="">Welcome</a>
+// <a routerLink="**">Not Found</a>
